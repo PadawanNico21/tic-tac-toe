4dd1a3d361d2fbc5d2e2c7ea01301466e74eb8f1
PadawanNico21 <63717504+PadawanNico21@users.noreply.github.com>
2024-02-04 10:51:27 +0100

    📦 Ajout des exports dans models

--- libs/models/src/index.ts
+++ libs/models/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/models.module'
+export * from './lib/Users.service'
+export * from './lib/models/game-elo.model'
+export * from './lib/models/user.model'
--- libs/models/src/lib/models.module.ts
+++ libs/models/src/lib/models.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 import { PrismaSchemasModule } from '@tictactoe/prisma-schemas'
+import { UsersService } from './Users.service'
 
 @Module({
-    controllers: [],
-    providers: [],
-    exports: [],
+    providers: [UsersService],
+    exports: [UsersService],
     imports: [PrismaSchemasModule],
 })
 export class ModelsModule {}

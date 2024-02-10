522beed9a23b76bccc14bf310ad803d343dac913
PadawanNico21 <63717504+PadawanNico21@users.noreply.github.com>
2024-02-10 15:45:02 +0100

    ✨ Ajout de l'utilisation de ConfigService

--- libs/cryptography/src/lib/cryptography.factory.ts
+++ libs/cryptography/src/lib/cryptography.factory.ts
@@ -1,16 +1,22 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
 import { EncryptionAlgs } from '@tictactoe/prisma-schemas'
 import { ICryptoService } from './crypto-service.interface'
 import { Argon2IDChaCha20Poly1305Service } from './crypto-services/argon2id-chacha20poly1305.service'
+import { ConfigType } from '@nestjs/config'
+import { cryptoConfig } from '@tictactoe/backend-configuration'
 
 @Injectable()
 export class CryptographyFactory {
+    constructor(
+        @Inject(cryptoConfig.KEY)
+        private configService: ConfigType<typeof cryptoConfig>
+    ) {}
+
     createFromEnum(type: EncryptionAlgs): ICryptoService {
         switch (type) {
             case 'ARGON2ID_CHACHA20POLY1305':
-                //TODO: Add env provider
                 return new Argon2IDChaCha20Poly1305Service({
-                    secret: Buffer.from('TODO: Add env provider', 'utf-8'),
+                    secret: this.configService.secret,
                 })
             default:
                 throw new Error('Not Implemented.')
--- libs/cryptography/src/lib/cryptography.module.ts
+++ libs/cryptography/src/lib/cryptography.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common'
 import { CryptographyFactory } from './cryptography.factory'
+import { ConfigModule } from '@nestjs/config'
+import { cryptoConfig } from '@tictactoe/backend-configuration'
 
 @Module({
     controllers: [],
     providers: [CryptographyFactory],
     exports: [CryptographyFactory],
+    imports: [ConfigModule.forFeature(cryptoConfig)],
 })
 export class CryptographyModule {}

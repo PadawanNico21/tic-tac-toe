5d3118137f5d03f6b02dc6f30514750b5b4bc61c
PadawanNico21 <63717504+PadawanNico21@users.noreply.github.com>
2024-02-10 15:44:08 +0100

    ✨ Parsing de CRYPTO_SECRET

--- libs/backend-configuration/src/config/crypto.config.ts
+++ libs/backend-configuration/src/config/crypto.config.ts
@@ -1,5 +1,10 @@
 import { registerAs } from '@nestjs/config'
 
-export const cryptoConfig = registerAs('crypto', () => ({
-    secret: process.env['CRYPTO_SECRET'],
-}))
+export const cryptoConfig = registerAs('crypto', () => {
+    if (!process.env['CRYPTO_SECRET'])
+        throw new Error(
+            "La variable d'environement CRYPTO_SECRET doit être définie"
+        )
+
+    return { secret: Buffer.from(process.env['CRYPTO_SECRET'], 'base64') }
+})

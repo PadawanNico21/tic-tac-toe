ecfa9edcd333fd8811924c2623e50ad6bf11b058
PadawanNico21 <63717504+PadawanNico21@users.noreply.github.com>
2024-02-14 12:50:27 +0100

    🔧 Configuration du serveur

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,21 +1,21 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common'
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-
 import { AppModule } from './app/app.module'
+import { instrumentate } from '@tictactoe/otel-instrumentations'
+
+instrumentate('tictactoe-backend')
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
-    const globalPrefix = 'api'
-    app.setGlobalPrefix(globalPrefix)
-    const port = process.env.PORT || 3000
-    await app.listen(port)
+    const defaultVersion = '1'
+
+    app.enableVersioning({ defaultVersion, type: VersioningType.URI })
+    app.useGlobalPipes(new ValidationPipe())
+
+    const port = process.env.PORT ?? 3000
+    await app.listen(port, '0.0.0.0')
     Logger.log(
-        `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+        `🚀 Application is running on: http://0.0.0.0:${port}/v${defaultVersion}}`
     )
 }
 
